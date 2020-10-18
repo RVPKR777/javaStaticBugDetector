fbb0242aa17185cf3a4c0b25723e62adc48a16fb
RVPKR777 <pavan.03121996@gmail.com>
2020-10-18 16:11:32 -0400

    main update

--- src/main/java/bug_Detector/main.java
+++ src/main/java/bug_Detector/main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String args[]) {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("C://Users//pavan//Downloads//cloudstack-4.9");
 
         String dir = myObj.nextLine();  
             new Analyser(dir);
