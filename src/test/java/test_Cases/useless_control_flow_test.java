0e42ae1b1c8b3dc1d3fec3076f24d6ec53e79e79
RVPKR777 <pavan.03121996@gmail.com>
2021-01-05 16:54:39 -0500

    test case update

--- src/test/java/test_Cases/useless_control_flow_test.java
+++ src/test/java/test_Cases/useless_control_flow_test.java
@@ -26,3 +26,25 @@ public class useless_control_flow_test {
         });
     }
 }
+
+ */
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.assertNotEquals;
+import static test_Cases.TestHelper.captureOutput;
+
+public class useless_control_flow_test {
+    @Test
+    public void uselesscontrolflow() throws Exception {
+        String file = "src/files_To_Test/useless_control_flow.java";
+
+        captureOutput(new CaptureTest() {
+            @Override
+            public void test(ByteArrayOutputStream outContent, ByteArrayOutputStream errContent) throws Exception {
+                System.out.print(new Analyser(file));
+                String sample = outContent.toString().replaceAll("\\s+","");
+                assertNotEquals("", sample);
+            }
+        });
+    }
+}
